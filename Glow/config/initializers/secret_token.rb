8e3f1a8ac9ac074e9b14dfe5ab50805c683c924a
Shane Randolph <r.shane.randolph@gmail.com>
2013-05-28 21:18:39 -0500

    Initial Commit

---
+++ Glow/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glow::Application.config.secret_token = 'd9da68594671122211c7d4590a44f097c95ebeb68a62ab27e765c6009787f01fcbcd18b9948e238d2615cc46a5e7bb5e55fb7d2dfa1930dbee199caab906665c'
